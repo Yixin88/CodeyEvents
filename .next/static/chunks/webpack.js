28c8836d07b75afd77182266ad0e4e9fb6c5d5b5
Yixin <yixin88@hotmail.co.uk>
2023-05-23 19:39:28 +0100

    added _document.js to add default DOM layout and added lang='en'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45a64bc128f7b7a1"; }
+/******/ 		__webpack_require__.h = function() { return "e6dc533783ddb036"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
