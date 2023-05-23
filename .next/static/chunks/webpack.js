32823f3ae99181e621040dd29bc783201469b92a
Yixin <yixin88@hotmail.co.uk>
2023-05-23 19:20:57 +0100

    added general meta data to _app.js file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "761b99b8c2d1f07a"; }
+/******/ 		__webpack_require__.h = function() { return "2332f64f71004270"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
