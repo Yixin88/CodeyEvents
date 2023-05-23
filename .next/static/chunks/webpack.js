bb40179c7a93e24b67286c02083b1f25dbd5497d
Yixin <yixin88@hotmail.co.uk>
2023-05-23 16:31:45 +0100

    slug filter page now using getServerSideProps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "595abf4400dc4115"; }
+/******/ 		__webpack_require__.h = function() { return "7f265419d4c9df77"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
