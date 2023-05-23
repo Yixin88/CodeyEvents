938cf796e14c59a790032c91289014d861bb2841
Yixin <yixin88@hotmail.co.uk>
2023-05-23 15:07:39 +0100

    linked home page to firebase backend to get data

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "68997079856ba4de"; }
+/******/ 		__webpack_require__.h = function() { return "4040dddaa5ace2c7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/events/index.js
+++ pages/events/index.js
@@ -4,7 +4,7 @@ import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 import { useRouter } from 'next/router';
 
-export default function EventPage() {
+export default function EventPage(props) {
 
   const events = getAllEvents();
   const router = useRouter();
@@ -20,4 +20,30 @@ export default function EventPage() {
       <EventList items={events}/>
     </div>
   )
+}
+
+export async function getStaticProps() {
+
+  const response = await fetch("https://nextjs-course-4ba9d-default-rtdb.firebaseio.com/events.json")
+  const data = await response.json();
+
+  const transfromedArray = [];
+
+  for (const key in data) {
+    transfromedArray.push({
+      id: key,
+      title: data[key].title,
+      description:
+        data[key].description,
+      date: data[key].date,
+      image: data[key].image,
+      isFeatured: data[key].isFeatured,
+    })
+  }
+
+  console.log(transfromedArray)
+
+  return {
+    props: {events: transfromedArray}
+  }
 }
