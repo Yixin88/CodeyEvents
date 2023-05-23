1ba0365a2a11a8a8757a9b3003e69f5b32fcc486
Yixin <yixin88@hotmail.co.uk>
2023-05-23 16:10:57 +0100

    all event page is now using firebase and all prerendered

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { getAllEvents } from '../../dummy-data'
+import { getAllEvents } from '../../helpers/api-util'
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 import { useRouter } from 'next/router';
 
 export default function EventPage(props) {
 
-  const events = getAllEvents();
+  const events = props.events;
   const router = useRouter();
 
   function findEventHandler(year, month) {
@@ -23,27 +23,10 @@ export default function EventPage(props) {
 }
 
 export async function getStaticProps() {
-
-  const response = await fetch("https://nextjs-course-4ba9d-default-rtdb.firebaseio.com/events.json")
-  const data = await response.json();
-
-  const transfromedArray = [];
-
-  for (const key in data) {
-    transfromedArray.push({
-      id: key,
-      title: data[key].title,
-      description:
-        data[key].description,
-      date: data[key].date,
-      image: data[key].image,
-      isFeatured: data[key].isFeatured,
-    })
-  }
-
-  console.log(transfromedArray)
+  const allEvents = await getAllEvents();
 
   return {
-    props: {events: transfromedArray}
+    props: {events: allEvents},
+    revalidate: 60
   }
 }
