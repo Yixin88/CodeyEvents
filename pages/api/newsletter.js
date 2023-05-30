1032f9f6bfac3eb0cfe87f31531b1cb22bf3543b
Yixin <yixin88@hotmail.co.uk>
2023-05-30 19:13:35 +0100

    added error handling in newsletter sign up

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,17 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const client = await MongoClient.connect('mongodb+srv://ying:ying@cluster0.andzl1u.mongodb.net/events?retryWrites=true&w=majority');
+
+  return client
+}
+
+async function insectDocument(client, document) {
+  const db = client.db();
+
+  await db.collection('newsletter').insertOne(document)
+}
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -9,13 +21,21 @@ export default async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect('mongodb+srv://ying:ying@cluster0.andzl1u.mongodb.net/events?retryWrites=true&w=majority')
-    
-    const db = client.db();
+    let client
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({message: 'Connecting to the database failed!'})
+      return;
+    }
 
-    await db.collection('newsletter').insertOne({email: userEmail})
-
-    client.close();
+    try {
+      await insectDocument(client, {email: userEmail})
+      client.close();
+    } catch (error) {
+      res.status(500).json({message: 'Inserting data failed!'})
+      return;
+    }
 
 
     res.status(201).json({ message: 'Signed Up!'})
