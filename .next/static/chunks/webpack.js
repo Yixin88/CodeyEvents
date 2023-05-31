af14400bc11fb64a2fe0906bedbb9f5d21d55f5c
Yixin <yixin88@hotmail.co.uk>
2023-05-31 20:41:34 +0100

    added pop up notification for adding new comments

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "386ba750db072a1d"; }
+/******/ 		__webpack_require__.h = function() { return "d400afded3c4332b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/input/comments.js
+++ components/input/comments.js
@@ -1,28 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '../../store/notification-context';
 
 export default function Comments(props) {
   const { eventId } = props;
 
+  const notificationCtx = useContext(NotificationContext);
+
   const [showComments, setShowComments] = useState(false);
+  const [updateComments, setUpdateComments] = useState(false);
   const [comments, setComments] = useState([])
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
 
   useEffect(() => {
-    fetch(`/api/comments/${eventId}`)
-    .then(response => response.json())
-    .then(data => {
-      setComments(data.comments)
-    })
-  })
+    if (showComments) {
+      setIsFetchingComments(true)
+      fetch(`/api/comments/${eventId}`)
+      .then(response => response.json())
+      .then(data => {
+        setComments(data.comments)
+        setIsFetchingComments(false)
+      })
+    }
+  }, [showComments, updateComments])
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: 'Adding Comment...',
+      message: 'Uploading your comment...',
+      status: 'pending'
+    })
+
     fetch(`/api/comments/${eventId}`, {
       method: 'POST',
       body: JSON.stringify(commentData),
@@ -30,9 +45,29 @@ export default function Comments(props) {
         'Content-Type': 'application/json'
       }
     })
-    .then(response => response.json())
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return response.json().then(data => {
+        throw new Error(data.message || 'Something went wrong!');
+      })
+    })
     .then(data => {
-      console.log(data)
+      notificationCtx.showNotification({
+        title: 'Success!',
+        message: 'Your comment was saved!',
+        status: 'success'
+      })
+      setUpdateComments(!updateComments)
+    })
+    .catch(error => {
+      notificationCtx.showNotification({
+        title: 'Error!',
+        message: error.message || 'Something went wrong!',
+        status: 'error'
+      })
     })
   }
 
@@ -42,7 +77,8 @@ export default function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments}/>}
+      {showComments && !isFetchingComments && <CommentList items={comments}/>}
+      {showComments && isFetchingComments && <><p>Loading...</p><CommentList items={comments}/></>}
     </section>
   );
 }
